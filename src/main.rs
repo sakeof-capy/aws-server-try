1da6b10581e172e2efbe42ef5f4ad7efa0e1b6bc
Fourier2718281828 <non-linear@ukr.net>
2024-06-13 04:59:45 +0300

    init

---
+++ src/main.rs
@@ -0,0 +1,43 @@
+use actix_web::{
+    get, 
+    post, 
+    web, 
+    web::Json,
+    App, 
+    HttpResponse, 
+    HttpServer, 
+    Responder
+};
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[get("/json")]
+async fn json() -> impl Responder {
+    Json("SOME JSON STRING".to_string())
+}
+
+#[post("/echo")]
+async fn echo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
+
+async fn manual_hello() -> impl Responder {
+    HttpResponse::Ok().body("Hey there!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+            .service(echo)
+            .service(json)
+            .route("/hey", web::get().to(manual_hello))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
